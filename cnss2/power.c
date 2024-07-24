4c80dafbdfe998edfbbc0f0d861f3979085b501b
Abhishek Mantha <quic_pmantha@quicinc.com>
2024-07-24 14:44:07 +0530

    cnss2: Fix array cnss_vreg_list unused variable compile error
    When defined CONFIG_CNSS2_X86, there will be a compile error of
    array cnss_vreg_list unused variable.
    
    Array cnss_vreg_list is only used when CONFIG_CNSS2_X86 is not
    defined. so correct macro CONFIG_CNSS2_X86 order for array
    cnss_vreg_list.
    
    Change-Id: I0a21ae904d9d8da136867541883cf210a05d690b

--- cnss2/power.c
+++ cnss2/power.c
@@ -24,6 +24,7 @@
 #include <linux/soc/qcom/qcom_aoss.h>
 #endif
 
+#ifndef CONFIG_CNSS2_X86
 #if IS_ENABLED(CONFIG_ARCH_QCOM)
 static struct cnss_vreg_cfg cnss_vreg_list[] = {
 	{"vdd-wlan-core", 1300000, 1300000, 0, 0, 0},
@@ -51,7 +52,6 @@ static struct cnss_clk_cfg cnss_clk_list[] = {
 	{"rf_clk", 0, 0},
 };
 #else
-#ifndef CONFIG_CNSS2_X86
 static struct cnss_vreg_cfg cnss_vreg_list[] = {
 };
 
