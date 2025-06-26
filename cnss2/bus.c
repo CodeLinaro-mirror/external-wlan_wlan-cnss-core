dd3fbaa297615da35581fbfc355784c63c6ebece
Jianfeng Zhu <jianfzh@qti.qualcomm.com>
2025-06-26 10:24:57 +0800

    cnss2: cologne cnss_get_bus_type support
    Change-Id: I8dab150422dde47e637459d060be32ef4c1330b1

--- cnss2/bus.c
+++ cnss2/bus.c
@@ -50,6 +50,7 @@ enum cnss_dev_bus_type cnss_get_bus_type(unsigned long device_id)
 	case QCN7605_VER20_COMPOSITE_DEVICE_ID:
 		return CNSS_BUS_USB;
 	case QCN7605_SDIO_DEVICE_ID:
+	case COLOGNE_SDIO_DEVICE_ID:
 		return CNSS_BUS_SDIO;
 	default:
 		cnss_pr_err("Unknown device_id: 0x%lx\n", device_id);
