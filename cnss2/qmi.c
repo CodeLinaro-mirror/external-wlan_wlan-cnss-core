a634d4ce69356b8c24d82d3dcfd0aa2bed8e9d6f
Zhaoyang Liu <zhaoyang@qti.qualcomm.com>
2025-09-12 10:54:14 +0800

    cnss2: add mlo capabality for cologne sdio
    Add mlo capability into QMI_WLFW_HOST_CAP_REQ_V01
    message to firmware for Cologne SDIO device.
    
    Change-Id: If19acb2a86c80ed7ec7e0f806d2aa96e24e4b059

--- cnss2/qmi.c
+++ cnss2/qmi.c
@@ -171,6 +171,33 @@ static int cnss_wlfw_clnt_svc_event_notifier(struct notifier_block *nb,
 	return ret;
 }
 
+static void cnss_wlfw_host_cap_parse_mlo(struct cnss_plat_data *plat_priv,
+					 struct wlfw_host_cap_req_msg_v01 *req)
+{
+	if (plat_priv->device_id == COLOGNE_SDIO_DEVICE_ID) {
+		req->mlo_capable_valid = 1;
+		req->mlo_capable = 1;
+		req->mlo_chip_id_valid = 1;
+		req->mlo_chip_id = 0;
+		req->mlo_group_id_valid = 1;
+		req->mlo_group_id = 0;
+		req->max_mlo_peer_valid = 1;
+		/* Max peer number generally won't change for the same device
+		 * but needs to be synced with host driver.
+		 */
+		req->max_mlo_peer = 32;
+		req->mlo_num_chips_valid = 1;
+		req->mlo_num_chips = 1;
+		req->mlo_chip_info_valid = 1;
+		req->mlo_chip_info[0].chip_id = 0;
+		req->mlo_chip_info[0].num_local_links = 2;
+		req->mlo_chip_info[0].hw_link_id[0] = 0;
+		req->mlo_chip_info[0].hw_link_id[1] = 0;
+		req->mlo_chip_info[0].valid_mlo_link_id[0] = 1;
+		req->mlo_chip_info[0].valid_mlo_link_id[1] = 1;
+	}
+}
+
 static int cnss_wlfw_host_cap_send_sync(struct cnss_plat_data *plat_priv)
 {
 	struct wlfw_host_cap_req_msg_v01 req;
@@ -214,6 +241,8 @@ static int cnss_wlfw_host_cap_send_sync(struct cnss_plat_data *plat_priv)
 	req.nm_modem_valid = 1;
 	req.nm_modem |= WLFW_HOST_CAP_INTERNAL_SLEEPCLOCK_MASK;
 	cnss_pr_dbg("nm_modem is %d\n", req.nm_modem);
+
+	cnss_wlfw_host_cap_parse_mlo(plat_priv, &req);
 
 	if (cnss_get_platform_name(plat_priv, req.platform_name,
 				   QMI_WLFW_MAX_PLATFORM_NAME_LEN_V01))
